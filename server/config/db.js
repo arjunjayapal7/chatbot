d2f064edd7a961da3be52d412b765c3996618513
arjun jayapal <arjun.p.jayapal@gmail.com>
2025-01-20 20:48:37 +0530

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+  } else {
+    console.log("Connected to the PostgreSQL database");
+  }
+});
+
+export default pool
